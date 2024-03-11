5fa55c93135323e748675643c44615c16824f5d7
Karan <karan_chgh@yahoo.com>
2024-03-10 23:04:52 -0400

    corrected path for modules

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # create vpc
 module "vpc" {
-  source                  = "../modules/vpc"
+  source                  = "./modules/vpc"
   region                  = var.region
   project_name            = var.project_name
   vpc_cidr                = var.vpc_cidr
@@ -16,7 +16,7 @@ module "vpc" {
 
 # create nat gateway
 module "natgateway" {
-  source                = "../modules/natgateway"
+  source                = "./modules/natgateway"
   public_subnet_az1_id  = module.vpc.public_subnet_az1_id
   internet_gateway      = module.vpc.internet_gateway
   public_subnet_az2_id  = module.vpc.public_subnet_az2_id
@@ -27,14 +27,14 @@ module "natgateway" {
 
 # create security group
 module "security_group" {
-  source = "../modules/security_group"
+  source = "./modules/security_group"
   vpc_id = module.vpc.vpc_id
 
 }
 
 # create alb
 module "application_load_balancer" {
-  source                = "../modules/alb"
+  source                = "./modules/alb"
   project_name          = module.vpc.project_name
   alb_security_group_id = module.security_group.alb_security_group_id
   public_subnet_az1_id  = module.vpc.public_subnet_az1_id
@@ -44,7 +44,7 @@ module "application_load_balancer" {
 
 #create rds
 module "rds" {
-  source                = "../modules/rds"
+  source                = "./modules/rds"
   vpc_id                = module.vpc.vpc_id
   alb_security_group_id = module.security_group.alb_security_group_id
   secure_subnet_az1_id  = module.vpc.secure_subnet_az1_id
@@ -53,7 +53,7 @@ module "rds" {
 
 # create ASG
 module "asg" {
-  source                    = "../modules/asg"
+  source                    = "./modules/asg"
   project_name              = module.vpc.project_name
   rds_db_endpoint           = module.rds.rds_db_endpoint
   private_subnet_az1_id     = module.vpc.private_subnet_az1_id
