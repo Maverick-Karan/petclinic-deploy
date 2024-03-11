85720909a16ad7df7b1ffafb41f7c38fcb46daac
Karan <karan_chgh@yahoo.com>
2024-03-11 09:45:15 -0400

    updated rds variables

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -2,6 +2,7 @@ variable "vpc_id" {}
 variable "secure_subnet_az1_id" {}
 variable "secure_subnet_az2_id" {}
 variable "alb_security_group_id" {}
+variable "ec2_security_group_id" {}
 variable "az1" {}
 variable "az2" {}
 variable "identifier" {}
