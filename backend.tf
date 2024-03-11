f732c77531758bec286128b8ded7027a24642561
Karan <karan_chgh@yahoo.com>
2024-03-10 23:09:41 -0400

    change backend region

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "pc-remotestate"
     key            = "default/terraform.tfstate"
-    region         = "us-east-2"
+    region         = "us-east-1"
     dynamodb_table = "pet-state-locking"
     encrypt        = "true"
   }
