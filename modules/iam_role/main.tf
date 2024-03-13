330d81290643b92df223e00cda28c9f055665a36
Karan <karan_chgh@yahoo.com>
2024-03-12 23:52:06 -0400

    added cloudwatch policy to ec2

--- modules/iam_role/main.tf
+++ modules/iam_role/main.tf
@@ -22,6 +22,12 @@ resource "aws_iam_role_policy_attachment" "ec2_role_policy" {
   role       = aws_iam_role.ec2_role.name
 }
 
+# Attach CloudWatchLogsFullAccess policy to the IAM role
+resource "aws_iam_role_policy_attachment" "ec2_role_cloudwatch_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+  role       = aws_iam_role.ec2_role.name
+}
+
 # Create an instance profile for the EC2 instance and associate the IAM role
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "EC2_SSM_Instance_Profile"
